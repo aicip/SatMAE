7993373b3e9bc88a551b819bc5528e90a3b72d1a
Andrei Cozma <me@andreicozma.com>
2023-02-12 22:01:52 -0500

    Fixed typo in pretraining shell scripts

--- train224.sh
+++ train224.sh
@@ -5,12 +5,13 @@ PATCH_SIZE=16
 BATCH_SIZE=64
 EPOCHS=200
 
-DEVICE="cuda:0"
+DEVICE="cuda:3"
 
-IN_PATH_BASE="/data2/HDD_16TB/fmow-rgb-preproc/"
+# IN_PATH_BASE="$HOME/Documents/datasets/image/fmow-rgb"
+IN_PATH_BASE="/data2/HDD_16TB/fmow-rgb-preproc"
 IN_PATH="$IN_PATH_BASE/train_$INPUT_SIZE.csv"
 
-OUT_DIR_BASE="./"
+OUT_DIR_BASE="."
 OUT_DIR="$OUT_DIR_BASE/out_i${INPUT_SIZE}_p${PATCH_SIZE}_b${BATCH_SIZE}_e${EPOCHS}"
 
-python3 main_pretrain.py --device $DEVICE --train_path $TRAIN_PATH --output_dir $OUT_DIR --input_size $INPUT_SIZE --patch_size $PATCH_SIZE --batch_size $BATCH_SIZE --epochs $EPOCHS
+python3 main_pretrain.py --device "$DEVICE" --train_path "$IN_PATH" --output_dir "$OUT_DIR" --input_size "$INPUT_SIZE" --patch_size "$PATCH_SIZE" --batch_size "$BATCH_SIZE" --epochs "$EPOCHS"
