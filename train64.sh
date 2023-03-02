2147fee996813b4816b68dcb703759c6dcc77717
Andrei Cozma <me@andreicozma.com>
2023-03-01 21:45:47 -0500

    Updates

--- train64.sh
+++ train64.sh
@@ -3,11 +3,13 @@
 DEVICE="cuda:0"
 
 EPOCHS=200
-BATCH_SIZE=64
+BATCH_SIZE=768
 
 INPUT_SIZE=64
-PATCH_SIZE=16
+PATCH_SIZE=8
 
+MODEL="mae_vit_large"
+FFN_NAME="FusedMLP"
 ATTENTION="scaled_dot_product"
 
 # IN_PATH_BASE="$HOME/Documents/datasets/image/fmow-rgb"
@@ -15,9 +17,7 @@ IN_PATH_BASE="/data2/HDD_16TB/fmow-rgb-preproc"
 IN_PATH="$IN_PATH_BASE/train_$INPUT_SIZE.csv"
 
 # OUT_DIR_BASE="."
-OUT_DIR_BASE="/data2/HDD_16TB/ICCV/Model_Saving"
-OUT_DIR="${OUT_DIR_BASE}/out_i${INPUT_SIZE}_p${PATCH_SIZE}_b${BATCH_SIZE}_e${EPOCHS}_${ATTENTION}"
+OUT_DIR_BASE="../Model_Saving"
+OUT_DIR="${OUT_DIR_BASE}/out_${MODEL}_i${INPUT_SIZE}_p${PATCH_SIZE}_b${BATCH_SIZE}_e${EPOCHS}_${ATTENTION}"
 
-WANDB="satmae"
-
-python3 main_pretrain.py --device "$DEVICE" --train_path "$IN_PATH" --output_dir "$OUT_DIR" --input_size "$INPUT_SIZE" --patch_size "$PATCH_SIZE" --batch_size "$BATCH_SIZE" --epochs "$EPOCHS" --attention "$ATTENTION" --wandb "$WANDB" $@
+python3 main_pretrain.py --device "$DEVICE" --train_path "$IN_PATH" --output_dir "$OUT_DIR" --model="$MODEL" --input_size "$INPUT_SIZE" --patch_size "$PATCH_SIZE" --batch_size "$BATCH_SIZE" --epochs "$EPOCHS" --ffn_name="$FFN_NAME" --attention "$ATTENTION" $@
