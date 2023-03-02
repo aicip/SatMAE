570e67b07c8104deee049566f57dabd8c2209e77
Andrei Cozma <me@andreicozma.com>
2023-03-02 11:44:33 -0500

    Updates

--- train112.sh
+++ train112.sh
@@ -17,7 +17,7 @@ IN_PATH="$IN_PATH_BASE/train_$INPUT_SIZE.csv"
 
 # OUT_DIR_BASE="."
 OUT_DIR_BASE="../Model_Saving"
-OUT_DIR="${OUT_DIR_BASE}/out_${MODEL}_${ATTENTION}_i${INPUT_SIZE}_p${PATCH_SIZE}_b${BATCH_SIZE}_e${EPOCHS}"
+OUT_DIR="${OUT_DIR_BASE}/out_${MODEL}_${ATTENTION}_${FFN_NAME}_i${INPUT_SIZE}_p${PATCH_SIZE}_b${BATCH_SIZE}_e${EPOCHS}"
 
 WANDB="satmae"
 # WANDB="satmae_testing"
