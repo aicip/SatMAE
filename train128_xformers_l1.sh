466b09379521f194bfc50a7acb70b3761c907772
Andrei Cozma <me@andreicozma.com>
2023-03-05 21:37:11 -0500

    Updates

--- train128_xformers_l1.sh
+++ train128_xformers_l1.sh
@@ -9,6 +9,8 @@ PATCH_SIZE=16
 MODEL="mae_vit_base"
 FFN_NAME="MLP"
 
+LOSS="l1"
+
 ATTENTION=$1
 if [ -z "$ATTENTION" ]; then
     echo "Usage: $0 <attention> [additional flags]"
@@ -23,9 +25,9 @@ IN_PATH="${IN_PATH_BASE}/train_${INPUT_SIZE}.csv"
 
 # OUT_DIR_BASE="."
 OUT_DIR_BASE="../Model_Saving"
-OUT_DIR="${OUT_DIR_BASE}/out_${MODEL}_xformers_${ATTENTION}_${FFN_NAME}_i${INPUT_SIZE}_p${PATCH_SIZE}_b${BATCH_SIZE}_e${EPOCHS}"
+OUT_DIR="${OUT_DIR_BASE}/out_${MODEL}_xformers_${ATTENTION}_${FFN_NAME}_i${INPUT_SIZE}_p${PATCH_SIZE}_b${BATCH_SIZE}_e${EPOCHS}_${LOSS}"
 
 # Note: If you want to use additional flags, pass them when running the script.
 # Example: ./trainX.sh --wandb satmae --device "cuda:0"
 
-python3 main_pretrain.py --use-xformers --train_path "$IN_PATH" --output_dir "$OUT_DIR" --model="$MODEL" --input_size "$INPUT_SIZE" --patch_size "$PATCH_SIZE" --batch_size "$BATCH_SIZE" --epochs "$EPOCHS" --attn_name "$ATTENTION" --ffn_name="$FFN_NAME" $@
+python3 main_pretrain.py --use-xformers --train_path "$IN_PATH" --output_dir "$OUT_DIR" --model="$MODEL" --input_size "$INPUT_SIZE" --patch_size "$PATCH_SIZE" --batch_size "$BATCH_SIZE" --epochs "$EPOCHS" --loss "$LOSS" --attn_name "$ATTENTION" --ffn_name="$FFN_NAME" $@
