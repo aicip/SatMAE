6a28b1b08dbe52e24f60c0634d88427cf108e30b
drkostas <georgiou.kostas94@gmail.com>
2023-03-07 21:23:30 -0500

    Added resume option on shunted train script

--- train_shunted.sh
+++ train_shunted.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # !/bin/zsh
 
-DEVICE="cuda:0"
+DEVICE="cuda:3"
 
 EPOCHS=200
 
 INPUT_SIZE=64
-BATCH_SIZE=256
+BATCH_SIZE=128
 
 PATCH_SIZES="4|4"
 MASK_RATIO='0.75'
@@ -19,25 +19,29 @@ LOSS="l1"
 PRINT_LEVEL=1
 ATTENTION="shunted"
 
-# MODEL_NAME="shunted_2s_mae_vit_tiny"
+MODEL_NAME="shunted_2s_mae_vit_tiny"
 # MODEL_NAME="shunted_2s_mae_vit_mini"
 # MODEL_NAME="shunted_2s_mae_vit_small"
 # MODEL_NAME="shunted_2s_mae_vit_base"
-MODEL_NAME="cross_shunted_2s_mae_vit_tiny"
+# MODEL_NAME="cross_shunted_2s_mae_vit_tiny"
 
 # Data path for com1822:
-# IN_PATH_BASE="/data2/HDD_16TB"
-# IN_PATH="${IN_PATH_BASE}/fmow-rgb-preproc/train_${INPUT_SIZE}.csv"
+IN_PATH_BASE="/data2/HDD_16TB"
+IN_PATH="${IN_PATH_BASE}/fmow-rgb-preproc/train_${INPUT_SIZE}.csv"
 # Data path for com2044
-IN_PATH_BASE="/mnt/com1822_HDD_16TB"
-IN_PATH="${IN_PATH_BASE}/fmow-rgb-preproc/train_${INPUT_SIZE}_com2044.csv"
+# IN_PATH_BASE="/mnt/com1822_HDD_16TB"
+# IN_PATH="${IN_PATH_BASE}/fmow-rgb-preproc/train_${INPUT_SIZE}_com2044.csv"
 
 # Data path for com1822:
-# OUT_DIR_BASE="/data2/HDD_16TB/ICCV/Model_Saving"
+OUT_DIR_BASE="/data2/HDD_16TB/ICCV/Model_Saving"
 # Data path for com2044
-OUT_DIR_BASE="/mnt/com1822_HDD_16TB/ICCV/Model_Saving"
+# OUT_DIR_BASE="/mnt/com1822_HDD_16TB/ICCV/Model_Saving"
 
 OUT_DIR="${OUT_DIR_BASE}/out_${MODEL_NAME}_i${INPUT_SIZE}_p${PATCH_SIZES}_e${EPOCHS}_${ATTENTION}_ratio${MASK_RATIO}_lr${LR}_loss${LOSS}"
+
+RESUME="${OUT_DIR_BASE}/out__i64_p4|4_e200_shunted_ratio0.75_lr0.001_lossl1"
+RESUME="${RESUME}/checkpoint-55.pth"
+START_EPOCH=55
 
 WANDB="satmae"
 
@@ -55,4 +59,6 @@ python3 main_pretrain.py \
 --lr "${LR}" \
 --loss "${LOSS}" \
 --mask_ratio "${MASK_RATIO}" \
+--resume "${RESUME}" \
+--start_epoch "${START_EPOCH}" \
 --wandb "${WANDB}" $@
