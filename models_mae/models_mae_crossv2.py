42f8574e1a1b1226c181cb00b133be9751429b27
Andrei Cozma <me@andreicozma.com>
2023-03-13 18:17:34 -0400

    Updates

--- models_mae/models_mae_crossv2.py
+++ models_mae/models_mae_crossv2.py
@@ -59,74 +59,33 @@ class MaskedAutoencoderViTCrossV2(MaskedAutoencoderViT):
             )
         )
 
-        # T.RandomApply(
-        #     [
-        #         T.RandomResizedCrop(
-        #             size=(self.input_size, self.input_size),
-        #             scale=random_resized_crop_scale,
-        #             antialias=True,
-        #         )
-        #     ],
-        #     p=global_prob,
-        # )
-
-        # T.RandomApply(
-        #     [
-        #         T.ColorJitter(
-        #             color_jitter_intensity,
-        #             color_jitter_intensity,
-        #             color_jitter_intensity,
-        #             color_jitter_intensity,
-        #         )
-        #     ],
-        #     p=global_prob,
-        # )
-
-        # T.RandomApply(
-        #     [T.RandomRotation(degrees=rotation_degrees, expand=False)],
-        #     p=global_prob,
-        # )
-
-        # T.RandomApply(
-        #     [
-        #         T.RandomPerspective(
-        #             distortion_scale=perspective_distortion_scale, p=global_prob
-        #         )
-        #     ]
-        # )
-
-        # T.RandomApply(
-        #     [
-        #         T.GaussianBlur(
-        #             gaussian_blur_kernel_size,
-        #             sigma=(gaussian_blur_sigma, gaussian_blur_sigma),
-        #         )
-        #     ],
-        #     p=global_prob,
-        # )
-
-        # self.augment1 = nn.Sequential(*transforms)
-
     def forward(self, imgs, mask_ratio=0.75, mask_seed: int = None):
         if mask_seed is not None:
             torch.manual_seed(mask_seed)
 
+        # Cropped images
         imgs_crop_sm, imgs_crop_md = self.crop_sm(imgs), self.crop_md(imgs)
 
-        loss_crop_sm, _, _, latent_crop_sm = super().forward(
-            imgs_crop_sm, mask_ratio=mask_ratio, mask_seed=mask_seed, return_latent=True
-        )
-        loss_crop_md, _, _, latent_crop_md = super().forward(
-            imgs_crop_md, mask_ratio=mask_ratio, mask_seed=mask_seed, return_latent=True
-        )
+        # Forward Original image
         loss_orig, pred_orig, mask_orig, latent_orig = super().forward(
             imgs, mask_ratio=mask_ratio, mask_seed=mask_seed, return_latent=True
         )
+        # Forward Small crop
+        loss_crop_sm, _, _, latent_crop_sm = super().forward(
+            imgs_crop_sm, mask_ratio=mask_ratio, mask_seed=mask_seed, return_latent=True
+        )
+        # Forward Medium crop
+        loss_crop_md, _, _, latent_crop_md = super().forward(
+            imgs_crop_md, mask_ratio=mask_ratio, mask_seed=mask_seed, return_latent=True
+        )
 
+        # Reducing losses for reconstruction based on decoder predictions
         losses_pred = [loss_orig, loss_crop_sm, loss_crop_md]
+        losses_pred_reduced = sum(losses_pred)
+        if self.losses_pred_reduction == "mean":
+            losses_pred_reduced /= len(losses_pred)
 
-        # latent loss between each crop and original
-        # as well as between crops themselves
+        # Latent loss function
         latent_loss_fn = None
         if self.loss_latent == "mse":
             latent_loss_fn = nn.MSELoss(reduction="mean")
@@ -135,23 +94,23 @@ class MaskedAutoencoderViTCrossV2(MaskedAutoencoderViT):
         else:
             raise ValueError(f"Unknown latent loss: {self.latent_loss}")
 
+        # latent loss between each crop and original
+        # as well as between crops themselves
         losses_latent = [
             latent_loss_fn(latent_crop_sm, latent_orig),
             latent_loss_fn(latent_crop_md, latent_orig),
             latent_loss_fn(latent_crop_sm, latent_crop_md),
         ]
 
-        print(f"losses_pred: {losses_pred}")
-        print(f"losses_latent: {losses_latent}")
-
-        losses_pred_reduced = sum(losses_pred)
-        if self.losses_pred_reduction == "mean":
-            losses_pred_reduced /= len(losses_pred)
-
+        # Reducing losses for latent space from encoder output
         losses_latent_reduced = sum(losses_latent)
         if self.lossed_latent_reduction == "mean":
             losses_latent_reduced /= len(losses_latent)
 
+        # Merging losses:
+        # - minimizing distance between latent spaces of crops and original (weighted)
+        # and
+        # - minimizing reconstruction loss for decoder predictions
         loss_final = (
             losses_pred_reduced + self.loss_latent_weight * losses_latent_reduced
         )
